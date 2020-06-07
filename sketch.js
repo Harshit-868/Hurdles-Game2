c60412154b03c0ba0d3c8f2d0e956d454c49b9e6
Harshit-868 <61592605+Harshit-868@users.noreply.github.com>
2020-06-07 11:49:26 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -55,7 +55,7 @@ function draw() {
     fill("black");
     textSize(50);
     textStyle(BOLD);
-    text(winner + " wins the race!", displayWidth/2, 100);
+    text(winner + " wins the race!", displayWidth, 100);
     if (!r && gameState === 2) {
       location.reload();
     }
